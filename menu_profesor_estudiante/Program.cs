3b8fb6ac62340a65c62e1a16ab9a25e04b50f8b9
dc552 <dc552@DESKTOP-80ATPO4>
2024-02-12 20:51:45 -0400

    Agregar archivos de proyecto.

---
+++ menu_profesor_estudiante/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+    public class Persona
+{
+    public string? Nombre { get; set; }
+    public string? Direccion { get; set; }
+    public string? Correo { get; set; }
+
+    public virtual void ImprimirDatos()
+    {
+        Console.WriteLine($"Nombre: {Nombre}\nDirección: {Direccion}\nCorreo: {Correo}");
+    }
+}
+
+
+public class Estudiante : Persona
+{
+    public string? Carrera { get; set; }
+
+    public override void ImprimirDatos()
+    {
+        base.ImprimirDatos();
+        Console.WriteLine($"Carrera: {Carrera}");
+    }
+}
+
+public class Profesor : Persona
+{
+    public string? Asignatura { get; set; }
+
+    public override void ImprimirDatos()
+    {
+        base.ImprimirDatos();
+        Console.WriteLine($"Asignatura: {Asignatura}");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Estudiante estudiante = new Estudiante();
+        Profesor profesor = new Profesor();
+
+
+        Console.WriteLine("Introduce los datos del Estudiante:");
+        estudiante.Nombre = Console.ReadLine();
+        estudiante.Direccion = Console.ReadLine();
+        estudiante.Correo = Console.ReadLine();
+        estudiante.Carrera = Console.ReadLine();
+
+        Console.WriteLine("\nDatos del estudiante:");
+        estudiante.ImprimirDatos();
+        Console.WriteLine("");
+
+        Console.WriteLine("Introduce los datos del Profesor:");
+        profesor.Nombre = Console.ReadLine();
+        profesor.Direccion = Console.ReadLine();
+        profesor.Correo = Console.ReadLine();
+        profesor.Asignatura = Console.ReadLine();
+
+        Console.WriteLine("\nDatos del profesor:");
+        profesor.ImprimirDatos();
+    }
+}
